775dd0628299cd98f7979ad309741bb71e18a204
Bob Kwiencien <bkwiencien@gmail.com>
2017-08-23 11:40:23 -0400

    added constructor

--- assets/javascript/trainscheduler.js
+++ assets/javascript/trainscheduler.js
@@ -1,18 +1,34 @@
 var connectAS ="";
+var train = {
+	trainName:  "",
+	destination: "",
+	frequency:   0,
+	nextArrival: "",
+	minutesAway:  0
+	};
+function Train(name) {
+	this.trainName = name;
+};
 var timeStarted = new Date()
 var arrayOfTrains = [];
 function clerk() {
-  console.log("clerk");
-  console.log(timeStarted);
   connectAs = "c";
   $("#buttons").remove();
   $("#status").text("connected as clerk");
+  createTrains();
 }
 function administrator() {
-  console.log("administrator");
-  console.log(timeStarted);
   connectAs = "a";
   $("#buttons").remove();
   $("#status").text("connected as administrator");
+  createTrains();
+
+}
+function createTrains() {
+	var t = new Train("cleveland");
+	arrayOfTrains.push(t);
+	console.log(arrayOfTrains.length);
+	console.log(arrayOfTrains[0].trainName)
+
 
 }
