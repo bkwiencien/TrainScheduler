6ef828ee45ea7b17af15bd0e2a5ec9d89e7a5b30
Bob Kwiencien <bkwiencien@gmail.com>
2017-08-23 10:37:28 -0400

    added connection status

--- assets/javascript/trainscheduler.js
+++ assets/javascript/trainscheduler.js
@@ -1,8 +1,18 @@
 var connectAS ="";
+var timeStarted = new Date()
+var arrayOfTrains = [];
 function clerk() {
   console.log("clerk");
+  console.log(timeStarted);
+  connectAs = "c";
+  $("#buttons").remove();
+  $("#status").text("connected as clerk");
 }
 function administrator() {
   console.log("administrator");
+  console.log(timeStarted);
+  connectAs = "a";
+  $("#buttons").remove();
+  $("#status").text("connected as administrator");
 
 }
