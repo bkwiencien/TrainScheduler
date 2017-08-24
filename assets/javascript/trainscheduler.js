99ee03635b5a51fc8495109981ad6adfb0705ab7
Bob Kwiencien <bkwiencien@gmail.com>
2017-08-24 16:44:39 -0400

    working on time

--- assets/javascript/trainscheduler.js
+++ assets/javascript/trainscheduler.js
@@ -34,25 +34,35 @@ function createTrains() {
 	minaway = generateMinutes()
 	var currentDate = new Date();
 	var minAway = generateMinutes();
-	calculateArrivalTime();
 	arrayOfTrains.push(t);
 	for (j=0;j<arrayOfTrainNames.length;j++) {
+	  var ariveAt = calculateArrivalTime(minAway);	
 	  var freq = generateFrequence();
 	  var t = new Train(arrayOfTrainNames[j],arrayOfDestinations[j],freq,"",minAway);
 	  arrayOfTrains.push(t);
     }
- for (k=1;k<arrayOfTrains.length;k++){
- 	console.log("in loop " + arrayOfTrains[k].trainName);
- 	console.log("in loop " + arrayOfTrains[k].destination);
- 	console.log("in loop " + arrayOfTrains[k].frequency);
- }
+ //for (k=1;k<arrayOfTrains.length;k++){
+// 	console.log("in loop " + arrayOfTrains[k].trainName);
+// 	console.log("in loop " + arrayOfTrains[k].destination);
+// 	console.log("in loop " + arrayOfTrains[k].frequency);
+// }
 
 function generateMinutes() {
 	var tempnum = Math.round(Math.random()*100);
 	return(tempnum);
 }
-function calculateArrivalTime () {
-	return(0);
+function calculateArrivalTime (aw) {
+	var d = (new Date() + aw*1000);
+	var dd = new Date();
+	console.log("right now it is " + dd);
+	console.log(d);
+	//var year  = d.getFullYear();
+ 	//var month = d.getMonth();
+ 	//var day   = d.getDay();
+ 	//var hour  = d.getHour();
+    //var minute   = d.getMinute();
+    console.log(d);
+	return(" ");
 }
 function generateFrequence () {
 	var tempnum = Math.round(Math.random()*1000);
